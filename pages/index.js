385fc59312b964b4ec78456dd0296134b41b359f
Eduardo-cremm <138538705+Eduardo-cremm@users.noreply.github.com>
2025-11-22 00:17:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Dudinha se voce me ama, da uma risadinha :) </h1>
+function Home() {
+  return <h1>Dudinha se voce me ama, da uma risadinha :) </h1>;
 }
-
 
 export default Home;
